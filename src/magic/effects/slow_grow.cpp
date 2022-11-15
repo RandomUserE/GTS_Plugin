c115be582625c5e84af157fdc479e3e621328f9c
RandomUserE <118186457+RandomUserE@users.noreply.github.com>
2022-11-15 04:52:34 -0500

    Slow Grow Bonus

--- src/magic/effects/slow_grow.cpp
+++ src/magic/effects/slow_grow.cpp
@@ -40,7 +40,7 @@ namespace Gts {
 			bonus = get_target_scale(caster) * 0.25 + 0.75;
 		}
 
-		Grow(caster, 20, power * bonus);
+		Grow(caster,power * bonus, 0.0);
 		GrowthTremorManager::GetSingleton().CallRumble(caster, caster, 0.30);
 		//log::info("Slowly Growing, actor: {}", caster->GetDisplayFullName());
 	}
